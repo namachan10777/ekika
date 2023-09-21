94b5630f68b9985fa6b0cc7539d64d5846863dd3
Masaki Nakano <admin@namachan10777.dev>
2023-09-22 00:48:05 +0900

    fix clippy warnings

--- activity-vocabulary/src/core/object.rs
+++ activity-vocabulary/src/core/object.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{xsd::LangContainer, *};
 
-///	Describes an object of any kind.
+/// Describes an object of any kind.
 /// The Object type serves as the base type for
 /// most of the other kinds of objects defined in the Activity Vocabulary,
 /// including other Core types such as [Activity], [IntransitiveActivity], [Collection] and [OrderedCollection].
