29dbd08a5bd36b4b955e37d05e5b9e69ac470c2a
Masaki Nakano <admin@namachan10777.dev>
2023-09-24 06:39:37 +0900

    reorganize

--- activity-vocabulary/README.md
+++
@@ -1,3 +0,0 @@
-# activity-vocabulary
-
-This is an implementation of [Activity Vocabulary 2.0](https://www.w3.org/TR/activitystreams-vocabulary/).
--- activity-vocabulary/src/vocab/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod activity;
-pub mod actor;
-pub mod core;
-pub mod object_and_link;
-pub use self::activity::*;
-pub use self::actor::*;
-pub use self::core::*;
-pub use self::object_and_link::*;
